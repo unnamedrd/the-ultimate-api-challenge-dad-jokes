91af96654b611ce91dbd002120fce38699756803
najwanan <najwanan@hotmail.com>
2022-03-20 18:25:10 -0700

    initialize XMLHttpRequest

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,3 +1,20 @@
+
+const API_ENDPOINT = "https://icanhazdadjoke.com/";
+const request = new XMLHttpRequest();
+const joke = document.querySelector('#joke');
+const button = document.querySelector('#button');
+const loader = document.querySelector('')
+
+request.addEventListener("load", getDadJokes);
+request.open("GET", API_ENDPOINT);
+var data = JSON.parse(xhr.responseText);
+request.send();
+
+
+function getDadJokes() {
+    
+}
+
 /**
  * 1. Initialize an XMLHttpRequest constructor
  * 2. Open a GET request, set the headers and response type
@@ -8,4 +25,4 @@
  * 7. Bonus: change button CTA to indicate if it's the first joke or a "next" one
  */
 
-const API_ENDPOINT = 'https://icanhazdadjoke.com/';
+
